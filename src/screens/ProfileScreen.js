ddc1a61fbc4bb94b55d653eb090d331e1c2136bf
JakubStani <kuba.staniszewski.ks@gmail.com>
2023-11-23 01:37:22 +0100

    add content to profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import SideBar from '../shared/SideBar';
+import NavBar from '../shared/NavBar';
+import './ProfileScreen.css';
 
-function ProfileScreen() {
+function ProfileScreen(props) {
   return (
-    <div>ProfileScreen</div>
+    <SideBar
+      sideBarOpen={props.sideBarOpen}
+      setSideBarOpen={props.setSideBarOpen}
+      toggleSideBar={props.toggleSideBar}
+      content={
+        <div className="App">
+          <header className="App-header">
+            <NavBar toggleSideBar={props.toggleSideBar}/>
+            <div className='profile-title-container'>Profile</div>
+            <div className='profile-data-container'>
+                <div className='profile-specific-data-container'>
+                    <p>First name</p>
+                    <p>test</p>
+                </div>
+                <div className='profile-specific-data-container'>
+                    <p>Last name</p>
+                    <p>test</p>
+                </div>
+                <div className='profile-specific-data-container'>
+                    <p>Email</p>
+                    <p>test</p>
+                </div>
+            </div>
+            <div className='profile-action-container'>
+                <div>
+                    <p>Edit</p>
+                </div>
+            </div>
+
+            <div className='profile-action-container'>
+                <div>
+                    <p>Change password</p>
+                </div>
+            </div>
+
+            <div className='profile-action-container'>
+                <div>
+                    <p>Delete account</p>
+                </div>
+            </div>
+          </header>
+        </div>
+      }
+    />
   );
 };
 
