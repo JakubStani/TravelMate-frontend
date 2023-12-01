99d5e8253aa1894c2848f48819435a760dff2078
JakubStani <kuba.staniszewski.ks@gmail.com>
2023-12-01 21:53:12 +0100

    get trips data from server

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,56 @@
 import NavBar from "../shared/NavBar";
 import SideBar from "../shared/SideBar";
 import SharedPlan from "../components/SharedPlan";
+import { List } from "react-virtualized";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 function Home(props) {
 
+  const endpoint = 'https://travelmatebackend.azurewebsites.net/api/v1/trips/browse';
+
+  // Nagłówki, które chcesz dodać do żądania
+const headers = {
+  'Authorization': `Bearer ${localStorage.getItem('userToken')}`,
+};
+
+  const [sharedPlansData, setSharedPlansData]= useState([]);
+
   //changing site's title
   document.title = `TravelMate - Home`;
+
+  
+  useEffect(() => {
+    /*fetch('https://travelmatebackend.azurewebsites.net/api/v1/trips/browse', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('userToken')}`
+
+          },
+          redirect: 'follow',
+          //mode: 'no-cors'
+        }).then(response => response.text())
+        .then(result => {
+          console.log("result1", result);
+          const jsonResult = JSON.parse(result);
+          console.log("result2", jsonResult);
+          setSharedPlansData(jsonResult);
+        })
+        .catch(error => console.log('error', error));*/
+
+          axios.get(endpoint, {headers})
+          .then(response => {
+            // Otrzymaj dane z odpowiedzi
+            console.log('Response:', response.data);
+            // Tutaj możesz przetworzyć otrzymane dane
+          })
+          .catch(error => {
+            // Obsługa błędu
+            console.error('Error:', error);
+          });
+  }, []);
+
 
   return (
     <SideBar
