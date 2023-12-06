21e60849bb8323e725c487f5a5f2bcdf43ca0e03
JakubStani <kuba.staniszewski.ks@gmail.com>
2023-12-06 19:57:12 +0100

    change navbar structure and styling

--- src/shared/NavBar.js
+++ src/shared/NavBar.js
@@ -4,17 +4,30 @@ import { toggleSideBar } from "./SideBar";
 
 const NavBar = (props) => {
     return (  
-        <div className="nav_container">
-            <nav className="navbar">
+        <header className="navbar">
+            <nav className="nav_container">
                 <div className="routes">
-                    <Link to={'/home'}>Logo</Link>
-                    <Link to={'/home'}>Shared plans</Link>
-                    <Link >Create plan</Link>
-                    <Link onClick={()=> props.toggleSideBar()}>Profile Icon</Link>
+                    <a href="/home">Logo</a>
+                    <a href="/home">Shared plans</a>
+                    <a href="/create-plan">Create plan</a>
+                    <a onClick={()=> props.toggleSideBar()}>Profile Icon</a>
+
                 </div>
             </nav>
-        </div>
+        </header>
     );
 };
  
 export default NavBar;
+
+
+// <div className="nav_container">
+        //     <nav className="navbar">
+        //         <div className="routes">
+        //             <Link to={'/home'}>Logo</Link>
+        //             <Link to={'/home'}>Shared plans</Link>
+        //             <Link to={'/create-plan'}>Create plan</Link>
+        //             <Link onClick={()=> props.toggleSideBar()}>Profile Icon</Link>
+        //         </div>
+        //     </nav>
+        // </div>
