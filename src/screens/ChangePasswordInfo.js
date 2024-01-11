7a808fb9a023787470576777a9e585e5b507797f
JakubStani <kuba.staniszewski.ks@gmail.com>
2024-01-11 16:54:11 +0100

    add ChangePasswordInfo

---
+++ src/screens/ChangePasswordInfo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChangePasswordInfo() {
+  return (
+    <div>Dear user, please check your email and enter the link to change your password</div>
+  )
+}
+
+export default ChangePasswordInfo
