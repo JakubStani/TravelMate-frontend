f687deda659417c7aae9c60b371121e98a7fc408
JakubStani <kuba.staniszewski.ks@gmail.com>
2024-01-13 14:34:00 +0100

    remove SideBar, Navbar, some comments and unnecessary code from ChangePassword.js

--- src/screens/ChangePassword.js
+++ src/screens/ChangePassword.js
@@ -8,7 +8,6 @@ function ChangePassword(props) {
 
   const navigate=useNavigate();
 
-  const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmationPassword, setConfirmationPassword] = useState('');
 
@@ -16,22 +15,14 @@ function ChangePassword(props) {
 
   const submitPasswordChange = (event) =>{
       event.preventDefault();
-      const newPasswordData = {
-          "currentPassword": currentPassword,
-          "newPassword": newPassword,
-          "confirmationPassword": confirmationPassword
-      };
 
       fetch(`https://travelmatebackend.azurewebsites.net/api/v1/auth/resetPassword?token=${token}`, {
         method: 'POST',
         body: JSON.stringify({newPassword: newPassword}),
         headers: {
           'Content-Type': 'application/json',
-          //'Authorization': `Bearer ${localStorage.getItem('userToken')}`
-
         },
         redirect: 'follow',
-        //mode: 'no-cors'
       }).then(response => response.text())
       .then(result => {
         console.log("result1", result);
@@ -43,49 +34,40 @@ function ChangePassword(props) {
 
   //TODO: check if user is logged in or not and adjust view to it
   return (
-    <SideBar
-      sideBarOpen={props.sideBarOpen}
-      setSideBarOpen={props.setSideBarOpen}
-      toggleSideBar={props.toggleSideBar}
-      content={
-        <div className="App">
-            <header>
-                <NavBar toggleSideBar={props.toggleSideBar}/>
+    <div className="App">
+        <header>
+            <div className='change-password-title-container'>Change password</div>
 
-                <div className='change-password-title-container'>Change password</div>
+            <div className='shared-plan-data-container'>
+                <form onSubmit={submitPasswordChange}>
+                    <div name="newPassword" className="input">
+                    <input 
+                        type="text" 
+                        required
+                        placeholder="New password"
+                        value={newPassword}
+                    onChange={(value) => setNewPassword(value.target.value)}
+                        ></input>
+                    </div>
 
-                <div className='shared-plan-data-container'>
-                    <form onSubmit={submitPasswordChange}>
-                        <div name="newPassword" className="input">
-                        <input 
-                            type="text" 
-                            required
-                            placeholder="New password"
-                            value={newPassword}
-                        onChange={(value) => setNewPassword(value.target.value)}
-                            ></input>
-                        </div>
+                    <div name="confirmedNewPassword" className="input">
+                    <input 
+                        type='text' 
+                        required
+                        placeholder="Confirme new password"
+                        value={confirmationPassword}
+                        onChange={(value) => setConfirmationPassword(value.target.value)}
+                        ></input>
+                    </div>
 
-                        <div name="confirmedNewPassword" className="input">
-                        <input 
-                            type='text' 
-                            required
-                            placeholder="Confirme new password"
-                            value={confirmationPassword}
-                            onChange={(value) => setConfirmationPassword(value.target.value)}
-                            ></input>
-                        </div>
+                    <div className="submit-container">
+                    <button className="submit-button">Submit</button>
+                    </div>
+                </form>
 
-                        <div className="submit-container">
-                        <button className="submit-button">Submit</button>
-                        </div>
-                    </form>
-
-                </div>
-            </header>
-        </div>
-      }
-    />
+            </div>
+        </header>
+    </div>
   );
 }
 
