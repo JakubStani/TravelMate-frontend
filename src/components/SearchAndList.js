f77102cfa3d9ea320c9ff47f6b1092d5dfad1989
JakubStani <kuba.staniszewski.ks@gmail.com>
2024-01-13 14:36:04 +0100

    create SearchAndList component

---
+++ src/components/SearchAndList.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import SearchBar from '../shared/SearchBar';
+import { List } from 'react-virtualized';
+import './SearchAndList.css'
+
+function SearchAndList(props) {
+    
+  return (
+    <div className='users-card-data-container'>
+                        <div>
+                            <SearchBar 
+                                search={(event)=> (event.key ==="Enter"? props.getData(): null)}
+                                whenTextChanges={(e)=> {props.setSearchedUser(e.target.value);}}
+                                searchedFriend={props.searchedUser}
+                                />
+                            <div>
+                                <List
+                                    width={window.innerWidth*0.35}
+                                    height={window.innerHeight*0.4}
+                                    rowCount={props.usersData.length}
+                                    rowHeight={70}
+                                    rowRenderer={props.renderUser}
+                                    />
+                            </div>
+                        </div>
+                    </div>
+  )
+}
+
+export default SearchAndList
